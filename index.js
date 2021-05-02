d6a5ad9a3daca58b41704d2e0ce383dbba063a4b
lekipising <leeplanke@gmail.com>
2021-05-02 14:19:55 +0200

    consume api

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ $(document).ready(function() {
     });
   });
 
+$(document).ready(() => {
+
+$.ajax({
+    url: "http://localhost:5000/premises", 
+    method: 'GET',
+    success: function(res){
+        if(res.rows.length > 0){
+            for(let index = 0; index < res.rows.length; index++) {
+                var newRow = $("<tr>");
+                var cols = "";
+                var premiseId = '';
+                var MeterNo = '';
+                var Customerid = '';
+                var RID = '';
+                cols += '<td> '+ res.rows[index].premiseId +'</td>';
+                cols += '<td> '+ res.rows[index].MeterNo +'</td>';
+                cols += '<td> '+ res.rows[index].Customerid+'</td>';                
+                newRow.append(cols);
+                $("#allpr .tbody").append(newRow);
+            }  
+
+        }
+    }
+})
+})
