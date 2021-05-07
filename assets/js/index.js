7bb3ff3a4c494a190764f38018f7f8f16d000cfd
lekipising <leeplanke@gmail.com>
2021-05-07 23:20:22 +0200

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@
 
 var selectedRecord = null;
 var selectedRecordID = null;
-var baseUrl = "http://localhost:5000";
+var baseUrl = "https://waterrw.herokuapp.com";
 
 // Get cookie
 function getCookie(name) {
